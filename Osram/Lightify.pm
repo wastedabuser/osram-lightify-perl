d13f4defcfeb7816b695efe68dd6aa3820594414
Andrey Glavchev <wastedabuser@gmail.com>
2016-10-08 17:03:56 +0300

    Convert API to class

---
+++ Osram/Lightify.pm
@@ -0,0 +1,110 @@
+package Osram::Lightify;
+
+use JSON;
+use LWP::UserAgent;
+use HTTP::Request;
+
+sub new {
+	my ($class, %args) = @_;
+	my $self = {%args};
+	bless $self, $class;
+
+	return $self;
+}
+
+sub output {
+	my ($self, $str) = @_;
+	return unless $self->{debug};
+	print "$str\n";
+}
+
+sub authenticate {
+	my ($self) = @_;
+	my $result = $self->callApiPost(
+		'/services/session',
+		{   username     => $self->{username},
+			password     => $self->{password},
+			serialNumber => $self->{serialNumber}
+		}
+	);
+	$self->{token} = $result->{securityToken};
+	return $result;
+}
+
+sub callApiPost {
+	my ($self, $url, $data) = @_;
+	my $host    = "$self->{host}$url";
+	my $content = encode_json($data);
+	$self->output('========= API post =========');
+	$self->output($host);
+	$self->output($content);
+
+	my $req = HTTP::Request->new('POST', $host);
+	$req->content_type('application/json');
+	$req->header(authorization => $self->{token}) if $self->{token};
+	$req->content($content);
+
+	my $ua       = LWP::UserAgent->new;
+	my $response = $ua->request($req);
+	if ($response->is_success) {
+		$self->output($response->content);
+		return decode_json($response->content);
+	} else {
+		die $response->status_line.": $url";
+	}
+}
+
+sub callApiGet {
+	my ($self, $url, $data) = @_;
+	my $host = "$self->{host}$url";
+	$self->output('========= API get =========');
+	$self->output($host);
+	$host .= join('&', map { "$_=$data->{$_}" } keys %$data) if $data;
+
+	my $req = HTTP::Request->new('GET', $host);
+	$req->content_type('application/json');
+	$req->header(authorization => $self->{token}) if $self->{token};
+
+	my $ua       = LWP::UserAgent->new;
+	my $response = $ua->request($req);
+	if ($response->is_success) {
+		$self->output($response->content);
+		return decode_json($response->content);
+	} else {
+		die $response->status_line.": $url";
+	}
+}
+
+sub getGroups {
+	my ($self) = @_;
+	return @{ $self->{groups} } if $self->{groups};
+	return @{ $self->{groups} = $self->callApiGet('/services/groups') };
+}
+
+sub groupByName {
+	my ($self, $name) = @_;
+	return (grep { $_->{name} =~ /$name/i } $self->getGroups)[0];
+}
+
+sub findGroupId {
+	my ($self, $name) = @_;
+	return $self->groupByName($args{g})->{groupId};
+}
+
+sub getDevices {
+	my ($self) = @_;
+	return @{ $self->{devices} } if $self->{devices};
+	return @{ $self->{devices} = $self->callApiGet('/services/devices') };
+}
+
+sub deviceByName {
+	my ($self, $name) = @_;
+	return (grep { $_->{name} =~ /$name/i } $self->getDevices)[0];
+}
+
+sub findDeviceId {
+	my ($self, $name) = @_;
+	return $self->deviceByName($args{d})->{deviceId};
+}
+
+1;
--- lightify.pl
+++ lightify.pl
@@ -2,9 +2,9 @@ use strict;
 
 use JSON;
 use Data::Dumper;
-use LWP::UserAgent;
-use Eldhelm::Util::FileSystem;
 use Eldhelm::Util::CommandLine;
+use Eldhelm::Util::FileSystem;
+use Osram::Lightify;
 
 my $cmd = Eldhelm::Util::CommandLine->new(
 	argv => \@ARGV,
@@ -16,6 +16,8 @@ my $cmd = Eldhelm::Util::CommandLine->new(
 		[ 'g',      'group by name' ],
 		[ 'on',     'on' ],
 		[ 'off',    'off' ],
+		[ 'lg',     'list groups' ],
+		[ 'ld',     'list devices' ],
 		[ 'debug',  'shows messages' ],
 		[ 's',      'security token' ]
 	]
@@ -31,106 +33,25 @@ if ($args{h} || $args{help}) {
 my $cfgStream = Eldhelm::Util::FileSystem->getFileContents('config.json');
 my $config    = decode_json($cfgStream);
 
-sub output($) {
-	my ($str) = @_;
-	return unless $args{debug};
-	print "$str\n";
-}
+my $api = Osram::Lightify->new(%$config, debug => $args{debug}, token => $args{s});
 
-my $token = $args{s};
-
-sub callApiPost {
-	my ($url, $data) = @_;
-	my $host    = "$config->{host}$url";
-	my $content = encode_json($data);
-	output($host);
-	output($content);
-
-	my $req = HTTP::Request->new('POST', $host);
-	$req->content_type('application/json');
-	$req->header(authorization => $token) if $token;
-	$req->content($content);
-
-	my $ua       = LWP::UserAgent->new;
-	my $response = $ua->request($req);
-	if ($response->is_success) {
-		output($response->content);
-		return decode_json($response->content);
-	} else {
-		die $response->status_line.": $url";
-	}
-}
-
-sub callApiGet {
-	my ($url, $data) = @_;
-	my $host = "$config->{host}$url";
-	output($host);
-	$host .= join('&', map { "$_=$data->{$_}" } keys %$data) if $data;
-
-	my $req = HTTP::Request->new('GET', $host);
-	$req->content_type('application/json');
-	$req->header(authorization => $token) if $token;
-
-	my $ua       = LWP::UserAgent->new;
-	my $response = $ua->request($req);
-	if ($response->is_success) {
-		output($response->content);
-		return decode_json($response->content);
-	} else {
-		die $response->status_line.": $url";
-	}
-}
-
-my $groups;
-
-sub getGroups() {
-	return @$groups if $groups;
-	return @{ $groups = callApiGet('/services/groups') };
-}
-
-sub groupByName {
-	my ($name) = @_;
-	return (grep { $_->{name} =~ /$name/i } getGroups)[0];
-}
-
-sub findGroupId {
-	my ($name) = @_;
-	return groupByName($args{g})->{groupId};
-}
-
-my $devices;
-
-sub getDevices() {
-	return @$devices if $devices;
-	return @{ $devices = callApiGet('/services/devices') };
-}
-
-sub deviceByName {
-	my ($name) = @_;
-	return (grep { $_->{name} =~ /$name/i } getDevices)[0];
-}
-
-sub findDeviceId {
-	my ($name) = @_;
-	return deviceByName($args{d})->{deviceId};
-}
-
-unless ($token) {
-	my $authData = callApiPost(
-		'/services/session',
-		{   username     => $config->{username},
-			password     => $config->{password},
-			serialNumber => $config->{serialNumber}
-		}
-	);
-	$token = $authData->{securityToken};
-	die 'authentication failed' unless $token;
+unless ($api->{token}) {
+	$api->authenticate;
+	die 'Authentication failed' unless $api->{token};
 }
 
 if ($args{on}) {
-	callApiGet('/services/group/set?idx='.findGroupId($args{g}).'&onoff=1')   if $args{g};
-	callApiGet('/services/device/set?idx='.findDeviceId($args{d}).'&onoff=1') if $args{d};
+	$api->callApiGet('/services/group/set?idx='.$api->findGroupId($args{g}).'&onoff=1')   if $args{g};
+	$api->callApiGet('/services/device/set?idx='.$api->findDeviceId($args{d}).'&onoff=1') if $args{d};
 } elsif ($args{off}) {
-	callApiGet('/services/group/set?idx='.findGroupId($args{g}).'&onoff=0')   if $args{g};
-	callApiGet('/services/device/set?idx='.findDeviceId($args{d}).'&onoff=0') if $args{d};
+	$api->callApiGet('/services/group/set?idx='.$api->findGroupId($args{g}).'&onoff=0')   if $args{g};
+	$api->callApiGet('/services/device/set?idx='.$api->findDeviceId($args{d}).'&onoff=0') if $args{d};
+}
+
+if ($args{lg}) {
+	warn Dumper $api->getGroups;
+}
+
+if ($args{ld}) {
+	warn Dumper $api->getDevices;
 }
